503e54bbee49e611d962d9c9530bcc9800a5721d
shokhrukhKarimov <shohruxkarimov546@gmail.com>
2023-08-27 17:51:40 +0500

    let's learn something new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+
+function ChildComponent({ onClick }) {
+  console.log("ChildComponent rendering");
+  return <button onClick={onClick}>Click me</button>;
+}
+
+function ParentComponent() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Shokhrukh");
+
+  // Without useCallback, the onClick function would change on every render
+  // causing ChildComponent to re-render unnecessarily.
+  const handleClick = useCallback(() => {
+    setCount((prevCount) => prevCount + 1);
+  }, []);
+
+  // const handleClick = () => {
+  //   setCount((prevCount) => prevCount + 1);
+  // };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <p>Name: {name}</p>
+      <button onClick={() => setName((n) => n + "a")}>change name</button>
+      <ChildComponent onClick={handleClick} />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ParentComponent />
     </div>
   );
 }
